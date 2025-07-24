ef006e0490d666eec07a71c9ca8d0460eb9d3572
Ifaz Howlader <ifaz.howl@gmail.com>
2025-07-23 22:04:35 -0400

    Updated finished code

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-let items = []
+let myLeads = []
 const inputEl = document.getElementById("input-el")
-const btnEl = document.getElementById("btn-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-btnEl.addEventListener("click", function() {
-    items.push(inputEl.value)
-    inputEl.value = ""
-    renderItems()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
-
-function renderItems() {
-    let itemsList = ""
-    for (let i = 0; i < items.length; i++) {
-        itemsList += 
-        `<li>
-            <a target=_blank href='${items[i]}'>
-                ${items[i]}
-            </a>
-        </li>`
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEl.innerHTML = itemsList
+    ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
